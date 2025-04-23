5bdf9ce9c360b2d65c0c003431ecbd0f4ba6b12a
shankkarpal46 <shankkarpal46@gmail.com>
2025-04-23 08:13:36 +0530

    file-handling(practice2)->append,unlinkSync and mkdir()

--- Node/file.js
+++ Node/file.js
@@ -19,4 +19,17 @@ const fs = require('fs')
 //     }
 // })
 
-fs.appendFileSync("./test.txt",new Date().getDate().toLocaleString())
+// fs.appendFileSync("./test.txt",new Date().getDate().toLocaleString())
+
+// fs.appendFileSync("./test.txt",`\n Hey There`)
+
+// fs.appendFileSync("./test.txt",`\n ${Date.now()} Hey there`)
+
+// fs.unlinkSync("./copy.txt")
+
+console.log(fs.statSync("./test.txt")) //to check the stats
+console.log(fs.statSync("./test.txt").isFile()) //to check whether it is file or not
+
+// fs.mkdirSync("my-docs") //creating a folder or directory
+
+fs.mkdirSync("my-docss/a/b", {recursive:true})
