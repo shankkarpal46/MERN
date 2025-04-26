4bb6b49c7bfb058f0258a8c4bc18d14e0c823c87
shankkarpal46 <shankkarpal46@gmail.com>
2025-04-26 14:38:23 +0530

    Routing in API's

--- Express/rest_api.js
+++ Express/rest_api.js
@@ -20,14 +20,47 @@ app.get("/users",(req,res)=>{
     res.send(html)
 })
 
-// GET /api/users/:id -> Dynamic id 
-app.get("/api/users/:id",(req,res)=>{
+app.route("/api/users/:id")
+.get((req,res)=>{
     const id = Number(req.params.id)
     
     const user = users.find(user => user.id === id)
 
     return res.json(user)
 })
+.patch((req,res)=>{
+    // To Edit a user with id
+    return res.json({status:"pending"})
+})
+.delete((req,res)=>{
+    // To Delete a user with id
+    return res.json({status:"pending"})
+})
+
+
+// GET /api/users/:id -> Dynamic id 
+// app.get("/api/users/:id",(req,res)=>{
+//     const id = Number(req.params.id)
+    
+//     const user = users.find(user => user.id === id)
+
+//     return res.json(user)
+// })
+
+app.post("/api/users",(req,res)=>{
+    // To Create a user
+    return res.json({status:"pending"})
+})
+
+// app.patch("/api/users",(req,res)=>{
+//     // To Edit a user
+//     return res.json({status:"pending"})
+// })
+
+// app.delete("/api/users",(req,res)=>{
+//     // To Delete a user
+//     return res.json({status:"pending"})
+// })
 
 app.listen(PORT,()=>console.log(`Server started on PORT 8000`))
 
